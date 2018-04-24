fc454dd5c0b592e3317627448cb21c45518f8a3b
Nathan Chowning <nathan@chowning.me>
2018-04-23 23:07:59 -0400

    Added some extra logic to the dummy test app

--- app/wrapper.sh
+++ app/wrapper.sh
@@ -4,4 +4,4 @@ ip route del default
 ip route add default via 10.5.0.3
 
 # Command that the container should actually run
-while true;do sleep 5;done
+while true;do curl -s --connect-timeout 2 icanhazip.com;sleep 10;done
